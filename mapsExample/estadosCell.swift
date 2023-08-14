609493013a6c61783f728be4ebf728dd99a992f8
vicYoshiok <k-on79@hotmail.com>
2023-08-14 12:36:05 -0600

    Adición de crashlytics para reporte de errores 2

--- mapsExample/estadosCell.swift
+++ mapsExample/estadosCell.swift
@@ -5,13 +5,14 @@
 //  Created by Vic Yoshioka on 13/08/23.
 //
 
+
 import Foundation
 import UIKit
 
 class estadosCell:UITableViewCell{
     
     @IBOutlet weak var idEstadoLabel: UILabel!
-    
     @IBOutlet weak var nombreEstadoLabel: UILabel!
+    
     
 }
--- mapsExample/modelPaises.swift
+++ mapsExample/modelPaises.swift
@@ -22,24 +22,3 @@ struct Estados{
 }
 
     
-
-/*
- tengo el array paises con objetos pais  que tienen la siguiente estructura     struct Pais {
-         var idPais: String
-         var nombrePais: String
-     }
- 
- y el array estados con objetos estado que tienen la siguiente estructura
- struct Estados{
-     
-     var idEstado: String
-     var estadoNombre: String
-     var coordenadas: String
-     var idPais: String
-     
- }
- 
- necesito una función que reciba del arreglo de estados el idPais y me devuelva del array paises el pais correspondiente
- 
- 
- **/
